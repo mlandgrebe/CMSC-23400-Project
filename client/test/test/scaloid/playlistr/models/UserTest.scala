7a01d6966813fa4135ee3e140ac297561d3a83e2
pscollins <pscollins@uchicago.edu>
2015-05-09 22:29:53 -0500

    let's figure out how to run tests

--- client/test/test/scaloid/playlistr/models/UserTest.scala
+++ client/test/test/scaloid/playlistr/models/UserTest.scala
@@ -1,10 +1,29 @@
 package scaloid.playlistr.models
 
+import argonaut.Parse
 import org.scalatest.FlatSpec
+import org.scalatest._
+import scaloid.playlistr.models.Models.User
 
 /**
  * Created by patrick on 5/9/15.
  */
-class UserTest extends FlatSpec {
+class UserTest extends FlatSpec with Matchers {
+  val testId = 22
+  val testURI = "uri"
+  val testName = "John"
+  val testUser = new User(testId, testURI, testName)
+  val userJSON = s"{userId=$testId, uri=\"$testURI\", name=\"$testName\""
 
+
+  "A user" should "know its parameters" in {
+    testUser.toParam should be ("userId", testId)
+  }
+
+  it should "be decodable from JSON" in {
+    val decoded: Option[User] = Parse.decodeOption[User](userJSON)
+
+    decoded should not be None
+    decoded.get shouldBe testUser
+  }
 }
