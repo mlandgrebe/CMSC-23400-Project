ec573fd591f4e35561b1764103e314d635f9fc41
pscollins <pscollins@uchicago.edu>
2015-05-10 19:47:44 -0500

    one passing test

--- client/test/scala/scaloid/playlistr/models/UserTest.scala
+++ client/test/scala/scaloid/playlistr/models/UserTest.scala
@@ -4,6 +4,7 @@ import argonaut.Parse
 import org.scalatest.FlatSpec
 import org.scalatest._
 import scaloid.playlistr.models.Models.User
+import scala.language.postfixOps
 
 /**
  * Created by patrick on 5/9/15.
@@ -17,7 +18,10 @@ class UserTest extends FlatSpec with Matchers {
 
 
   "A user" should "know its parameters" in {
-    testUser.toParam should be ("userId", testId)
+    println(testUser.toParam)
+    println(("userId", testId))
+    println(testUser.toParam == ("userId", testId))
+    testUser.toParam should equal (("userId", testId toString))
   }
 
   it should "be decodable from JSON" in {
