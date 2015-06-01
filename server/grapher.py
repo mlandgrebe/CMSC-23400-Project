2872d478e92296f755acf457d0e58c5f319a54f3
pscollins <pscollins@uchicago.edu>
2015-06-01 06:10:43 -0500

    different day, different library

--- server/grapher.py
+++ server/grapher.py
@@ -4,9 +4,9 @@ import pandas
 from flask.ext.mongoengine import mongoengine
 from mongoengine import Q
 from models import MotionInstant
+from datetime import datetime
 
-# http://stackoverflow.com/questions/24963062
-COLUMNS = ["norm", "time", "user"]
+COLUMNS = ["time", "norm", "user"]
 
 sns.set(style="darkgrid")
 
@@ -18,19 +18,44 @@ sns.set(style="darkgrid")
 #         times.append(mi.time)
 #     return (norms, times)
 
-def mis_for_song(song):
-    return MotionInstant.objects(
-        Q(time__gte=song.startTime) &
-        Q(time__lte=song.stopTime))
+def mis_for_song(song, extra_Q=None):
+    q = (Q(time__gte=song.startTime) &
+         Q(time__lte=song.stopTime))
+    if extra_Q:
+        q &= extra_Q
+    return MotionInstant.objects(q)
+
+def users_for_song(song):
+    return mis_for_song(song).distinct('user')
+
+def agg_mis_for_song(song):
+    users = users_for_song(song)
+    return {user.name:mis_for_song(song, extra_Q=Q(user=user))
+            for user in users}
+
+def fmt_time(dt):
+    return (dt - datetime(1970, 1, 1)).total_seconds()
+
+def extract(mis):
+    return [(fmt_time(m.time), m.norm, m.user.name) for m in
+            mis]
+
+def get_time(mis):
+    return (fmt_time(mi.time) for mi in mis)
+
+def get_norm(mis):
+    return (mi.norm for mi in mis)
+
+def get_both(mis):
+    return [(fmt_time(mi.time), mi.norm) for mi in mis]
 
 def df_for_song(song):
-    mis = MotionInstant.objects(
-        Q(time_gte=song.startTime) &
-        Q(time_lte=song.endTime))
-    df = pandas.DataFrame(mis, columns=COLUMNS)
+    mis = mis_for_song(song)
+    df = pandas.DataFrame(extract(mis), columns=COLUMNS)
     print df
     return df
 
 
-def plot(df):
-    return sns.tsplot(data=df, time="time", unit="user", value="norm")
+# def plot_df(df):
+#     return sns.tsplot(data=df, time="time",
+#                       condition="user", value="norm")
